611d8967ece5ee224782794bf30d5ef371499bc3
Mauro <redfish32@hotmail.it>
2020-05-30 17:42:22 +0200

    interactive menu

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const options = {
   };
 
 const client = opcua.OPCUAClient.create(options);
-let session
+let session;
+let subscription = [];
 
 
 async function menu(){
@@ -24,10 +25,11 @@ async function menu(){
         type: "list",
         name: "start_menu",
         message: "What do you want to do?",
-        choices: ["Create session","Read","Write","Browse","Make a subscription","Terminate program"],
+        choices: ["Create session","Read","Write","Browse","Make a subscription","Create monitored item","Terminate program"],
       }
     ]).then(answer => {
-      
+
+
       if(answer["start_menu"] == "Create session"){
         let status = clientJS.createSession(endpointUrl,client);
         status.then((value)=> {
@@ -45,7 +47,12 @@ async function menu(){
           start();
         })
       }
-      else if(answer["start_menu"] == "Write"){}
+      else if(answer["start_menu"] == "Write"){
+        let status = clientJS.write(session,opcua);
+        status.then((value) =>{
+          start();
+        }) 
+      }
       else if(answer["start_menu"] == "Browse"){
         let status = clientJS.browse(session);
         status.then((value) => {
@@ -56,7 +63,20 @@ async function menu(){
         })
         //start();
       }
-      else if(answer["start_menu"] == "Make a subscription"){}
+      else if(answer["start_menu"] == "Make a subscription"){
+        let status = clientJS.createSubscription(session,opcua);
+        status.then((value) =>{
+          subscription.push(value);
+          
+          start();
+        })
+      }
+      else if(answer["start_menu"] == "Create monitored item"){
+        let status = clientJS.monitorItem(opcua,subscription);
+        status.then((value) =>{
+          start();
+        })
+      }
       else if(answer["start_menu"] == "Terminate program"){
         process.exit();
       }
@@ -65,227 +85,6 @@ async function menu(){
   await clientJS.createConnection(endpointUrl,client);
   start();
 }
-
-
-
-
-  async function main() {
-    try {
-      // step 1 : connect to
-      await client.connect(endpointUrl);
-      console.log("Connection with server created!");
-      // step 2 : createSession
-      const session = await client.createSession();
-      console.log("New session created!");
-
-    
-      // step 3 : browse
-      var folders = [];
-      var root= "RootFolder"
-      navigator=root;
-      prev = root;
-      j = 0
-      do{
-            var choice = readline.question('Do you want to explore folders? y/n ');
-            
-            if(choice == "y"){
-                try{
-                  var browseResult = await session.browse(navigator);
-                  //console.log("TRY",browseResult);
-                }catch(err){
-                  try{
-                    var browseResult = await session.browse(navigator+"Folder");
-                    
-                  }catch(err){
-                    console.log(navigator+"Folder non è una directory");
-                    break;
-                  }
-                    //console.log("CATCH",browseResult);
-                }
-                  console.log("references of "+navigator+":");
-                var i = 0;
-                for (const reference of browseResult.references) {
-                                        
-                      folders.push(reference.browseName.toString());
-                      console.log("   -> ",i, reference.browseName.toString());
-                      i+=1;
-                }
-                if(folders.length==0){
-                  console.log("Directory vuota!")
-                }else{
-                    
-                direction = readline.question("Do you want return at parent directory? (y/n)");
-                if(direction=="y"){
-                  navigator=prev;
-                  folders=[];
-                }else{
-                  prev=navigator;
-                  choice1 = parseInt(readline.question("Put the number of the folder that you want to explore: "));
-                  navigator=folders[choice1];
-                  console.log("Stai scegliendo di navigare in ",navigator);
-                  //console.log("Prima ",folders);
-                  folders=[];
-                  //console.log("Dopo ",folders);
-                  //
-                }
-              }
-            }
-            
-     }while(choice == "y")
-    
-      
-  
-      //Read a variable
-      
-      do{
-        var choice = readline.question("Do you want to read? y/n ");
-        if(choice == "y"){
-          var maxAge = readline.question("Insert the max age ");
-          var nameSpaceIndex = readline.question("Insert the namespace Index of the node that you want to read ");
-          var nodeId = readline.question("Insert the node ID ");
-          const nodeToRead = {
-            //type NodeIdLike = string | NodeId | number;
-            nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
-            attributeId: opcua.AttributeIds.Value
-          };
-          const dataValue = await session.read(nodeToRead, maxAge);
-          console.log(dataValue.toString());
-          //console.log(dataValue.value);
-          //console.log(dataValue.statusCode.description);
-          
-
-        }else{break;}
-        
-      }while(choice == "y")
-
-
-      //WRITE
-      do{
-        choice = readline.question("Do you want to write a variable? y/n");
-        if(choice == "y"){
-          var nameSpaceIndex = readline.question("Insert the namespace Index of the node that you want to read --> ");
-          var nodeId = readline.question("Insert the node ID --> ");
-          var valueToWrite = readline.question("Insert the value to write --> ")
-
-          const nodeToWrite = {
-            //type NodeIdLike = string | NodeId | number;
-            nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
-            attributeId: opcua.AttributeIds.Value,
-            value:{
-              sourceTimestamp: new Date(),
-              statusCode: opcua.StatusCodes.Good,// <==== 
-              value:{
-                   dataType: opcua.DataType.Int32,
-                   value: valueToWrite
-              }
-          }
-          };
-          await session.write(nodeToWrite)
-        }
-      }while(choice=="y")
-
-  
-      // Create a subscription
-      do{
-          choice = readline.question("Do you want to create a subscription? y/n ");
-          if(choice == "y"){ 
-            //Range of time in which the server check the value
-            var requestedPublishingInterval = readline.question("Insert the requested publishing interval --> ");
-            //Lifetime of the subscription. It must be multiple of the publish interval
-            var requestedLifetimeCount = readline.question("Insert the requested lifetime count --> ");
-            //If the server has no notifications pending for the period of time defined by 
-            //(MaxKeepAliveCount * PublishingInterval), the server will send a keep alive message to the client.
-            var requestedMaxKeepAliveCount = readline.question("Insert the requested Max keep alive count --> ");
-            //The maximum number of notifications that the client wishes to receive in a single publish response. 
-            var maxNotificationPerPublish = readline.question("Insert the Max notification per publish --> ");
-            //If multiple subscriptions need to send notifications to the client, 
-            //the server will send notifications to the subscription with the highest priority first. 
-            var priority = readline.question("Insert the priority --> ");
-            var publishEnable = true
-
-            var subscription = opcua.ClientSubscription.create(session,{
-              requestedPublishingInterval: requestedLifetimeCount,
-              requestedLifetimeCount: requestedLifetimeCount,
-              requestedMaxKeepAliveCount: requestedMaxKeepAliveCount,
-              maxNotificationsPerPublish: maxNotificationPerPublish,
-              publishingEnabled: publishEnable,
-              priority: priority
-
-            })
-          
-            subscription.on("started", function() {
-              console.log("subscription started for 2 seconds - subscriptionId=",subscription.subscriptionId);
-            })
-              .on("keepalive", function() {console.log("keepalive");
-            })
-              .on("terminated", function() {console.log("terminated");
-            });
-            // install monitored item
-            
-            n = readline.question("Insert how many items you want to monitor");
-            var monitoredItemList = []
-            var monitor = []
-            console.log(monitoredItemList.length)
-            for(i = 0; i < n; i++){
-
-              var nameSpaceIndex = readline.question("Insert the namespace Index of the node that you want to read ");
-              var nodeId = readline.question("Insert the node ID ");
-
-              const itemToMonitor = {
-                nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
-                attributeId: opcua.AttributeIds.Value
-              };
-              const parameters = {
-                samplingInterval: 5000,
-                discardOldest: true,
-                queueSize: 10
-              };
-              
-              monitoredItemList.push(itemToMonitor);
-
-              monitor[i] = opcua.ClientMonitoredItem.create(
-                subscription,
-                monitoredItemList[i],
-                parameters,
-                opcua.TimestampsToReturn.Both
-              );
-                
-              monitor[i].on("changed", (dataValue) => {
-                console.log(" value has changed : ", dataValue.value.toString());
-              });
-                
-              
-            }     
-
-            async function timeout(ms) {
-              return new Promise(resolve => setTimeout(resolve, ms));
-            }
-            await timeout(10000);
-          
-
-            console.log("now terminating subscription");
-            await subscription.terminate();
-        }
-
-
-      }while(choice == "y")
-      
-      
-      
-      /*
-      // step 6: finding the nodeId of a node by Browse name
-      _"finding the nodeId of a node by Browse name"
-  
-      // close session
-      _"closing session"
-  
-      // disconnecting
-      _"disconnecting"
-      */
-    } catch(err) {
-      console.log("An error has occured : ",err);
-    }
-  } 
    
    //menu();
    menu();
--- opcua-client.js
+++ opcua-client.js
@@ -3,6 +3,18 @@ const readline = require('readline-sync');
 const inquirer = require("inquirer");
 const main = require('./main')
 
+
+/*
+  Defining costants  
+*/
+const DEFAULT_REQUEST_PUBLISH_INTERVAL = 1000;
+const DEFAULT_PRIORITY = 0;
+const DEFAULT_REQUESTED_MAX_KEEP_ALIVE_COUNT = 5;
+const DEFAULT_REQUESTED_LIFETIME_COUNT = 40;
+const DEFAULT_MAX_NOTIFICATION_PER_PUBLISH = 0;
+const DEFAULT_PUBLISH_ENABLE = 0;
+const DEFAULT_SAMPLING_INTERVAL = 1000;
+
 module.exports = {
 
     createConnection: async function(endpointUrl,client){
@@ -28,140 +40,182 @@ module.exports = {
 
     readNode: async function(opcua,session){
         
-        var maxAge = readline.question("Insert the max age ");
         
+        var test = readline.keyInPause("Press any key to continue...");
+        //console.log(test);
+        var maxAge = readline.question("Insert the max age ");
         var nameSpaceIndex = readline.question("Insert the namespace Index of the node that you want to read ");
         var nodeId = readline.question("Insert the node ID ");
+        
         const nodeToRead = {
             //type NodeIdLike = string | NodeId | number;
             nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
             attributeId: opcua.AttributeIds.Value
         };
         const dataValue = await session.read(nodeToRead, maxAge);
-        console.log(dataValue.toString());
+        
+        console.log("----------------------------------------------------");
+        console.log("DataType: "+opcua.DataType[dataValue.value.dataType]);
+        console.log("Value: "+ dataValue.value.value);
+        console.log("Status Code: "+dataValue.statusCode.name);
+        console.log("Source Timestamp: "+dataValue.sourceTimestamp);
+        console.log("Server Timestamp: "+dataValue.serverTimestamp);
+        console.log("----------------------------------------------------");
+       
+        //console.log(opcua.DataType[1])
         //console.log(dataValue.value);
         //console.log(dataValue.statusCode.description);
     },
 
     browse: async function(session,x){
-      // step 3 : browse
-      var folders = [];
-      var root= "RootFolder"
-      let val
-      navigator=root;
-      prev = root;
-      j = 0
+    },
+    
+    write: async function(session,opcua){
 
-      var browseResult = await session.browse(root);
-      var i = 0;
-      for (const reference of browseResult.references) {
-        folders.push(reference.browseName.toString());
-        i+=1;
-      }
-      console.log(folders);
-      folders.push("Back to menu");
+        var test = readline.keyInPause("Press any key to continue...");
+        var nameSpaceIndex = readline.question("Insert the namespace Index of the node that you want to read --> ");
+        var nodeId = readline.question("Insert the node ID --> ");
+        let valueToWrite = readline.question("Insert the value to write --> ")
 
-      val = inquirer.prompt([
-          {
-            type: "list",
-            name: "browse_menu",
-            message: "Select the directory in which you want to navigate",
-            choices: folders,
-          }
-      ]).then(async function parent(answer){
-          answer = answer["browse_menu"];
-          if(answer == "Back to menu"){
-              return("back");
-          }
-              
-              if(folders.length==0){
-                console.log("Directory vuota!")
-              }
-              folders.push("Back to menu");
-          
-      })
+        //Read the datatype from the node
+        const nodeToRead = {
+            //type NodeIdLike = string | NodeId | number;
+            nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
+            attributeId: opcua.AttributeIds.Value
+        };
+        let maxAge = 0;
+        const dataValue = await session.read(nodeToRead, maxAge);
+        let dataType = opcua.DataType[dataValue.value.dataType];
 
-        var i = 0;
-        // for (const reference of browseResult.references) {
-                                
-        //         folders.push(reference.browseName.toString());
-        //         console.log("   -> ",i, reference.browseName.toString());
-        //         i+=1;
-        // }
-        
-        if(folders.length==0){
-            console.log("Directory vuota!")
-        }else{
-            
-        direction = readline.question("Do you want return at parent directory? (y/n)");
-        if(direction=="y"){
-            navigator=prev;
-            folders=[];
-        }else{
-            prev=navigator;
-            choice1 = parseInt(readline.question("Put the number of the folder that you want to explore: "));
-            navigator=folders[choice1];
-            console.log("Stai scegliendo di navigare in ",navigator);
-            //console.log("Prima ",folders);
-            folders=[];
-            //console.log("Dopo ",folders);
-            //
-        }
-        }
-      
-      return val;
-      
-    /*
-      do{
-            var choice = readline.question('Do you want to explore folders? y/n ');
-            
-            if(choice == "y"){
-                try{
-                  var browseResult = await session.browse(navigator);
-                  //console.log("TRY",browseResult);
-                }catch(err){
-
-                    try{
-                        var browseResult = await session.browse(navigator+"Folder");
-                        
-                    }catch(err){
-                        console.log(navigator+"Folder non è una directory");
-                        break;
-                    }
-                    //console.log("CATCH",browseResult);
-                }
-                  console.log("references of "+navigator+":");
-                var i = 0;
-                for (const reference of browseResult.references) {
-                                        
-                      folders.push(reference.browseName.toString());
-                      console.log("   -> ",i, reference.browseName.toString());
-                      i+=1;
-                }
-                if(folders.length==0){
-                  console.log("Directory vuota!")
-                }else{
-                    
-                direction = readline.question("Do you want return at parent directory? (y/n)");
-                if(direction=="y"){
-                  navigator=prev;
-                  folders=[];
-                }else{
-                  prev=navigator;
-                  choice1 = parseInt(readline.question("Put the number of the folder that you want to explore: "));
-                  navigator=folders[choice1];
-                  console.log("Stai scegliendo di navigare in ",navigator);
-                  //console.log("Prima ",folders);
-                  folders=[];
-                  //console.log("Dopo ",folders);
-                  //
-                }
-              }
+        const nodeToWrite = {
+            //type NodeIdLike = string | NodeId | number;
+            nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
+            attributeId: opcua.AttributeIds.Value,
+            value:{
+            sourceTimestamp: new Date(),
+            statusCode: opcua.StatusCodes.Good,
+            value:{
+                    dataType: dataType,
+                    value: valueToWrite
             }
+            }
+        };
+        await session.write(nodeToWrite)
+
+    },
+
+    createSubscription: async function(session,opcua){
+        
+        choice = readline.keyInYN("Do you want to create a subscription?");
             
-     }while(choice == "y")
-     */
+                //Range of time in which the server check the value
+                var requestedPublishingInterval = readline.questionInt("Insert the requested publishing interval --> ");
+                if(requestedPublishingInterval == 0){
+                    requestedPublishingInterval = DEFAULT_REQUEST_PUBLISH_INTERVAL;
+                }
+
+                //If the server has no notifications pending for the period of time defined by 
+                //(MaxKeepAliveCount * PublishingInterval), the server will send a keep alive message to the client.
+                var requestedMaxKeepAliveCount = readline.question("Insert the requested Max keep alive count --> ");
+
+                //Lifetime of the subscription. It must be multiple of the publish interval
+                do{
+                    var requestedLifetimeCount = readline.questionInt("Insert the requested lifetime count" + 
+                     "(it must be at least 3*requestedMaxKeepAliveCount) --> ");
+                    if(requestedLifetimeCount == 0){
+                        requestedLifetimeCount = DEFAULT_REQUESTED_LIFETIME_COUNT;
+                    }
+                }while(requestedLifetimeCount < 3*requestedMaxKeepAliveCount);
+
+                
+                //The maximum number of notifications that the client wishes to receive in a single publish response. 
+                var maxNotificationPerPublish = readline.questionInt("Insert the Max notification per publish --> ");
+                if(maxNotificationPerPublish == 0){
+                    maxNotificationPerPublish = DEFAULT_MAX_NOTIFICATION_PER_PUBLISH;
+                }
+                //If multiple subscriptions need to send notifications to the client, 
+                //the server will send notifications to the subscription with the highest priority first. 
+                var priority = readline.questionInt("Insert the priority --> ");
+                if(priority == 0){
+                    priority == DEFAULT_PRIORITY;
+                }
+                var publishEnable = true
+
+                var subscription = opcua.ClientSubscription.create(session,{
+                requestedPublishingInterval: requestedLifetimeCount,
+                requestedLifetimeCount: requestedLifetimeCount,
+                requestedMaxKeepAliveCount: requestedMaxKeepAliveCount,
+                maxNotificationsPerPublish: maxNotificationPerPublish,
+                publishingEnabled: publishEnable,
+                priority: priority
+
+                })
+            
+                subscription.on("started", function() {
+                    console.log("\nSubscription started - subscriptionId=",subscription.subscriptionId);
+                    
+                })
+                .on("keepalive", function() {
+                })
+                .on("terminated", function() {console.log("Subscription terminated");
+                });
+                
+                return subscription;
+    },
+
+    monitorItem: async function(opcua,subscription){
+            
+            var monitor;
+            let subId = [];
+            for(i = 0; i < subscription.length; i++){
+                subId.push(subscription[i].subscriptionId.toString())
+            }
+            let subIndex = readline.keyInSelect(subId,"Select a subscriprion ID before to continue");
+            
+
+              var nameSpaceIndex = readline.questionInt("Insert the namespace Index of the node that you want to read ");
+              var nodeId = readline.questionInt("Insert the node ID ");
+
+              let samplingInterval = readline.questionInt("Insert the sampling interval"+ 
+              "(press 0 if you want default value: "+DEFAULT_SAMPLING_INTERVAL+"ms "+"---> ");
+              let queueSize = readline.questionInt("Insert the queue size ---> ");
+
+              const itemToMonitor = {
+                nodeId: "ns="+nameSpaceIndex+";i="+nodeId,
+                attributeId: opcua.AttributeIds.Value
+              };
+              const parameters = {
+                samplingInterval: samplingInterval,
+                discardOldest: true,
+                queueSize: queueSize
+              };
+              
+
+              monitor = opcua.ClientMonitoredItem.create(
+                subscription[subIndex],
+                itemToMonitor,
+                parameters,
+                opcua.TimestampsToReturn.Both
+              );
+                
+              monitor.on("changed", (dataValue) => {
+                console.log(" value has changed : ", dataValue.value.toString());
+              });
+                
+              
+            
+
+            async function timeout(ms) {
+              return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            await timeout(10000);
+          
+
+            console.log("now terminating subscription");
+            await subscription[subIndex].terminate();
     }
+
+    
 
 
 
